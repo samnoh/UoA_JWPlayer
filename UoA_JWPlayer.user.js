f91c7f8b0143d8658a3b05e31dfa3f833d108895
samnoh <samnoh21@outlook.com>
2018-10-18 14:37:11 +1300

    internal change

--- UoA_JWPlayer.user.js
+++ UoA_JWPlayer.user.js
@@ -2,46 +2,59 @@
 // @name         Make JW Player for UoA recordings Better
 // @namespace    Sam
 // @author       samnoh
-// @version      2.4.3
+// @version      2.5
 // @description  Add PiP function and change CSS
 // @icon
 // @match        https://raw.githubusercontent.com/samnoh/UoA_JWPlayer/master/UoA_JWPlayer.user.js
 // @include      https://mediastore.auckland.ac.nz/*
 // ==/UserScript==
 
-jwplayer().play(); // autoplay
-jwplayer().setCurrentQuality(0); // quality 0: HD, 1: SD, ...
-jwplayer().resize('100%', '100%'); // video size
-jwplayer('video').onReady( function () { // add pip button
-	jwplayer().addButton('','PiP', function() {
-		theVideo.webkitSetPresentationMode (theVideo.webkitPresentationMode === 'picture-in-picture' ? 'inline' : 'picture-in-picture');
-	}, 'fifth');
-});
+const jw = jwplayer();
+const quality = 0; // quality 0: HD, 1: SD, ...
 
-// video starts from 41 seconds to skip opening
-(!localStorage.getItem(window.location.href)) ? jwplayer().seek(41) : jwplayer().seek(localStorage.getItem(window.location.href));
+jw.play(); // autoplay
+jw.setCurrentQuality(quality); 
+jw.resize('100%', '100%'); // video size
+jw.addButton('','PiP', function() { // add pip button
+	theVideo.webkitSetPresentationMode (theVideo.webkitPresentationMode == 'picture-in-picture' ? 'inline' : 'picture-in-picture');
+}, 'fifth');
 
-// remember current position
+
+// video automatically starts from 41 seconds to skip the opening
+(!localStorage.getItem(window.location.href)) ? jw.seek(41) : jw.seek(localStorage.getItem(window.location.href));
+
+
+// auto save current position
 setInterval( function() {
-	jwplayer().getPosition() == jwplayer().getDuration() ? localStorage.removeItem(window.location.href) : localStorage.setItem(window.location.href, jwplayer().getPosition());
-}, 1000); // every one sec
+	jw.getPosition() == jw.getDuration() ? localStorage.removeItem(window.location.href) : localStorage.setItem(window.location.href, jw.getPosition());
+}, 1500); // every 1.5 seconds
+
 
 // title
-let url = window.location.href.split('/'); // current url
-let subject_title = url[5], date = url[8].split('.')[0].slice(0,10);
+const parseLink = (rec_link = '') => rec_link
+	.replace('https://mediastore.auckland.ac.nz/', '')
+	.split('/')
+	.filter((k, index) => {
+		return k.includes('.') || index == 2;
+	})
+	.reduce((acc, k, index) => {
+		index == 1 ? acc.push(k.split('.')[0]) : acc.push(k);
+		return acc;
+	},[]);
 
-// html title
-document.getElementsByTagName('title')[0].textContent = subject_title + ' ' + date.slice(6,8) + '/' + date.slice(4,6) + ' ' + date.slice(8,10) + ':00 Recording';
+const rec_url = parseLink(window.location.href); // current url
+const subject_title = rec_url[0], date = rec_url[1];
+document.getElementsByTagName('title')[0].textContent = subject_title + ' ' + date.slice(6,8) + '/' + date.slice(4,6) + ' ' + date.slice(8,10) + ':00';
+
 
 // body css
-let body_css = document.getElementsByTagName('body')[0];
-Object.assign( body_css.style, { 
+Object.assign(document.getElementsByTagName('body')[0].style, { 
 	backgroundColor: 'black' 
 });
 
+
 // video frame size
-let container_css = document.getElementsByClassName('container')[0];
-Object.assign( container_css.style, { 
+Object.assign(document.getElementsByClassName('container')[0].style, { 
 	backgroundColor: 'black',
 	width: '100%',
 	height: '100%' 
