494309ab9bde7462a56288e9c76627e5e5a14e82
samnoh <samnoh21@outlook.com>
2018-10-18 14:47:27 +1300

    change url var

--- UoA_JWPlayer.user.js
+++ UoA_JWPlayer.user.js
@@ -2,7 +2,7 @@
 // @name         Make JW Player for UoA recordings Better
 // @namespace    Sam
 // @author       samnoh
-// @version      2.5
+// @version      2.5.1
 // @description  Add PiP function and change CSS
 // @icon
 // @match        https://raw.githubusercontent.com/samnoh/UoA_JWPlayer/master/UoA_JWPlayer.user.js
@@ -11,22 +11,25 @@
 
 const jw = jwplayer();
 const quality = 0; // quality 0: HD, 1: SD, ...
+const bg_color = 'black';
+
+const rec_url = parseLink(window.location.href); // current url
 
 jw.play(); // autoplay
 jw.setCurrentQuality(quality); 
 jw.resize('100%', '100%'); // video size
-jw.addButton('','PiP', function() { // add pip button
+jw.addButton('', 'PiP', () => { // add pip button
 	theVideo.webkitSetPresentationMode (theVideo.webkitPresentationMode == 'picture-in-picture' ? 'inline' : 'picture-in-picture');
 }, 'fifth');
 
 
-// video automatically starts from 41 seconds to skip the opening
-(!localStorage.getItem(window.location.href)) ? jw.seek(41) : jw.seek(localStorage.getItem(window.location.href));
+// video automatically starts from 42 seconds to skip the opening
+(!localStorage.getItem(rec_url)) ? jw.seek(42) : jw.seek(localStorage.getItem(rec_url));
 
 
 // auto save current position
-setInterval( function() {
-	jw.getPosition() == jw.getDuration() ? localStorage.removeItem(window.location.href) : localStorage.setItem(window.location.href, jw.getPosition());
+setInterval( () => {
+	jw.getPosition() == jw.getDuration() ? localStorage.removeItem(rec_url) : localStorage.setItem(rec_url, jw.getPosition());
 }, 1500); // every 1.5 seconds
 
 
@@ -42,20 +45,20 @@ const parseLink = (rec_link = '') => rec_link
 		return acc;
 	},[]);
 
-const rec_url = parseLink(window.location.href); // current url
+
 const subject_title = rec_url[0], date = rec_url[1];
 document.getElementsByTagName('title')[0].textContent = subject_title + ' ' + date.slice(6,8) + '/' + date.slice(4,6) + ' ' + date.slice(8,10) + ':00';
 
 
 // body css
 Object.assign(document.getElementsByTagName('body')[0].style, { 
-	backgroundColor: 'black' 
+	backgroundColor: bg_color 
 });
 
 
 // video frame size
 Object.assign(document.getElementsByClassName('container')[0].style, { 
-	backgroundColor: 'black',
+	backgroundColor: bg_color,
 	width: '100%',
 	height: '100%' 
 });
